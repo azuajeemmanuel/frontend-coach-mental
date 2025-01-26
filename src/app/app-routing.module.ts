71725ed68f51a3602323ffc71e919c3ae225efe0
azuajeemanuel <azuajemmanuel@gmail.com>
2025-01-26 00:56:28 -0400

    sano

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,6 @@ import { LoginComponent } from './pages/login/login.component';
 import { AfiliacionesComponent } from './pages/afiliaciones/afiliaciones.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'index', pathMatch: 'full' },
   { path: 'index', component: IndexComponent },    
   { path: 'boton', component: ButtonDemoComponent },
   { path: 'contactanos', component: ContactanosComponent },
